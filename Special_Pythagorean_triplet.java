0297d98d5bb693983bcc59441b91821c614762e3
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-12-06 17:34:52 +0530

    Update Special_Pythagorean_triplet.java

--- Special_Pythagorean_triplet.java
+++ Special_Pythagorean_triplet.java
@@ -4,7 +4,7 @@ public class Special_Pythagorean_triplet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n=1000;
-		for(int i=1;i<=n;i++) {
+		for(int i=1;i<=n/3;i++) {
 			for(int j=i+1;j<=n/2;j++) {
 				int k=n-i-j;
 				if(i*i+j*j==k*k) {
